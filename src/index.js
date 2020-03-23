4a82ff7e7026bcfda825c53f3f7b6d485ce87584
dawsoncanby <dawsoncanby@gmail.com>
2020-03-22 21:20:21 -0600

    removed console.logs

--- src/index.js
+++ src/index.js
@@ -6,34 +6,34 @@ import { OBJLoader2 } from 'three/examples/jsm/loaders/OBJLoader2.js'
 
 import cow from './obj/cow.obj'
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-let controls = new OrbitControls(camera, renderer.domElement);
+let controls = new OrbitControls(camera, renderer.domElement)
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+var geometry = new THREE.BoxGeometry()
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+var cube = new THREE.Mesh(geometry, material)
+// scene.add(cube)
 
-const color = 0xFFFFFF;
-const intensity = 1;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(0, 10, 0);
-light.target.position.set(-5, 0, 0);
-scene.add(light);
-scene.add(light.target);
+const color = 0xFFFFFF
+const intensity = 1
+const light = new THREE.DirectionalLight(color, intensity)
+light.position.set(0, 10, 0)
+light.target.position.set(-5, 0, 0)
+scene.add(light)
+scene.add(light.target)
 
 camera.position.set(0, 20, 20)
 controls.target.set(0, 0, 0)
 controls.update()
 
 // instantiate a loader
-var loader = new OBJLoader2();
+var loader = new OBJLoader2()
 
 // load a resource
 loader.load(
@@ -41,17 +41,17 @@ loader.load(
 	cow,
 	// called when resource is loaded
 	(object) => { scene.add(object) },
-  xhr => { console.log(xhr); },
+  xhr => { },
   err => { console.log(err) }
 )
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-	requestAnimationFrame( animate );
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+	requestAnimationFrame( animate )
 
   controls.update()
 
-	renderer.render( scene, camera );
+	renderer.render( scene, camera )
 }
-animate();
+animate()
